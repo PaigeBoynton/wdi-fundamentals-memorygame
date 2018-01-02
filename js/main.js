64369f68971674871dc23eeb64c10d663e3bce3d
Paige Boynton <boynton.paige@gmail.com>
2018-01-02 18:19:55 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
-console.log("Up and Running!");
+console.log("up and running!");
+
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2) {
+var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
-}
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+};
+
+var flipCard = function(cardID) {
+	card = cards[cardID];
+	console.log("User flipped " + cards[cardID])
+	cardsInPlay.push(cards[cardID]);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+
+};
+
+flipCard(0);
+flipCard(1);
